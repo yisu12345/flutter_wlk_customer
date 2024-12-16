ebbd85c543ccd0b9ccf4c8ff9034fbd6e4386fad
wangliankun <wangliankun@meikeland.com>
2024-12-16 10:23:29 +0800

    feat(colors): JHYJColors

---
+++ lib/kola/colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class JHYJColors {
+  ///背景色
+  static const Color backgroundColor = Color(0xffF5F6F7);
+
+//  Color(0xffF0F4F8)
+  static const Color themeColor = Color(0xffF3C5BB);
+
+  //JHYJColors.themeTextColor
+  static const Color themeTextColor = Color(0xff1A1A1A);
+
+  static const Color appBarColor = Color(0xff4B67F8);
+
+  static const Color contentGreyColor = Color(0xffAAAAAA);
+
+  ///7
+  static const Color content7Color = Color(0xff777777);
+  static const Color contentF5Color = Color(0xfff5f5f5);
+  static const Color contentCCCColor = Color(0xffCCCCCC);
+  static const Color contentRedColor = Color(0xffE62222);
+
+  static const Color redTextColor = Color(0xffF84B4B);
+  static const Color contentE62222 = Color(0xffE62222);
+
+
+  //内容影响力颜色
+  static const List<Color> contentColor = [
+    Color(0xff0152C7),
+    Color(0xffF84B4B),
+    Color(0xffD25DD6),
+    Color(0xff5ED3E5),
+    Color(0xff4B67F8),
+    Color(0xff5CD1F7),
+  ];
+}
