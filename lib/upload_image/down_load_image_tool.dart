2e2cc21561675481764efcdebcf0a37a9bd1a877
wangliankun <wangliankun@meikeland.com>
2025-03-06 14:23:42 +0800

    feat(images): images download

--- lib/upload_image/down_load_image_tool.dart
+++ lib/upload_image/down_load_image_tool.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_wlk_customer/utils/toast_utils.dart';
 import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
+
 // import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -32,7 +33,10 @@ class DownLoadImageTool {
   }
 
   ///保存到相册
-  static Future<dynamic> savePhoto({required String imageUrl}) async {
+  static Future<dynamic> savePhoto({
+    required String imageUrl,
+    bool? isHideLoading,
+  }) async {
     //获取保存相册权限，如果没有，则申请改权限
     bool permition = await getPormiation();
 
@@ -40,8 +44,11 @@ class DownLoadImageTool {
     if (permition) {
       if (Platform.isIOS) {
         if (status.isGranted) {
-          var result =  imageRequest(imageUrl: imageUrl);
-           return result;
+          var result = imageRequest(
+            imageUrl: imageUrl,
+            isHideLoading: isHideLoading,
+          );
+          return result;
         }
         if (status.isDenied) {
           print("IOS拒绝");
@@ -49,7 +56,10 @@ class DownLoadImageTool {
       } else {
         //安卓
         if (status.isDenied) {
-          var result = imageRequest(imageUrl: imageUrl);
+          var result = imageRequest(
+            imageUrl: imageUrl,
+            isHideLoading: isHideLoading,
+          );
           return result;
         }
       }
@@ -60,18 +70,29 @@ class DownLoadImageTool {
     }
   }
 
-  static Future<dynamic> imageRequest({required String imageUrl}) async {
-    await EasyLoading.show(
-      // status: 'loading...',
-      maskType: EasyLoadingMaskType.black,
-    );
+  static Future<dynamic> imageRequest({
+    required String imageUrl,
+    bool? isHideLoading,
+  }) async {
+    if (isHideLoading == true) {
+    } else {
+      await EasyLoading.show(
+        // status: 'loading...',
+        maskType: EasyLoadingMaskType.black,
+      );
+    }
+
     var response = await Dio().get(
       imageUrl,
       options: Options(
         responseType: ResponseType.bytes,
       ),
     );
-    EasyLoading.dismiss();
+    if (isHideLoading == true) {
+    } else {
+      EasyLoading.dismiss();
+    }
+
     final result = await ImageGallerySaverPlus.saveImage(
         Uint8List.fromList(response.data),
         quality: 60,
