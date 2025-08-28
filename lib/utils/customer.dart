7b88d532221e881ad22f8403ee325ca5d76baea5
wangliankun <wangliankun@meikeland.com>
2025-08-28 15:23:09 +0800

    feat(html): html

--- lib/utils/customer.dart
+++ lib/utils/customer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 // import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -217,26 +217,26 @@ class CustomerHtmlWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    return Html(
-      data: html,
-      extensions: [
-        TagExtension(
-          tagsToExtend: {"flutter"},
-          child: const FlutterLogo(),
-        ),
-      ],
-      style: {
-        "p.fancy": Style(
-          textAlign: TextAlign.center,
-          // padding:  EdgeInsets.all(),
-          backgroundColor: Colors.grey,
-          margin: Margins(left: Margin(50, Unit.px), right: Margin.auto()),
-          width: Width(300, Unit.px),
-          fontWeight: FontWeight.bold,
-        ),
-      },
-    );
+    return HtmlWidget(html);
+    // return Html(
+    //   data: html,
+    //   extensions: [
+    //     TagExtension(
+    //       tagsToExtend: {"flutter"},
+    //       child: const FlutterLogo(),
+    //     ),
+    //   ],
+    //   style: {
+    //     "p.fancy": Style(
+    //       textAlign: TextAlign.center,
+    //       // padding:  EdgeInsets.all(),
+    //       backgroundColor: Colors.grey,
+    //       margin: Margins(left: Margin(50, Unit.px), right: Margin.auto()),
+    //       width: Width(300, Unit.px),
+    //       fontWeight: FontWeight.bold,
+    //     ),
+    //   },
+    // );
     // return Html(
     //   data:  html,
     //   style: const TextStyle(
