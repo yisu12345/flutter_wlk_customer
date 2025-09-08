abf641990a8a4c2bf15d761b21f81e3b03277470
wangliankun <wangliankun@meikeland.com>
2025-09-08 11:23:20 +0800

    feat(dash): 虚线

--- lib/lineline/dash_line.dart
+++ lib/lineline/dash_line.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 class DashLine extends StatelessWidget {
   final Color? color; // 虚线颜色
   final Axis direction; // 虚线方向
+  final double? dottedLength; //虚线长度
 
   const DashLine({
     Key? key,
     this.color,
     this.direction = Axis.horizontal,
+    this.dottedLength,
   }) : super(key: key);
 
   @override
@@ -17,6 +19,7 @@ class DashLine extends StatelessWidget {
       painter: DashedLinePainter(
         color: color ?? const Color(0xff36C2FD),
         direction: direction,
+        dottedLength: dottedLength,
       ),
     );
   }
@@ -26,12 +29,14 @@ class DashedLinePainter extends CustomPainter {
   final Color color;
   final double width;
   final Axis direction; //方向
+  final double? dottedLength; //虚线长度
 
   DashedLinePainter({
     this.width = 1,
     this.color = Colors.black,
     this.direction = Axis.horizontal,
-  });
+    this.dottedLength,
+});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -48,14 +53,15 @@ class DashedLinePainter extends CustomPainter {
           direction == Axis.horizontal ? size.width : 0,
           direction == Axis.horizontal ? 0 : size.height,
         ),
+      length: dottedLength,
     );
     canvas.drawPath(dottedPath, paint);
   }
 
-  Path _createDashedPath(Path path) {
+  Path _createDashedPath(Path path,{double? length}) {
     Path targetPath = Path();
-    double dottedLength = 10;
-    double dottedGap = 10;
+    double dottedLength = length ?? 10;
+    double dottedGap = length ?? 10;
     for (var metric in path.computeMetrics()) {
       double distance = 0;
       bool isDrawDotted = true;
